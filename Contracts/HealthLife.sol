d87cc5fa52e4f43ea5f20ad8adb89100b73c73fd
Richard Henderson <64328714+HendersonRichardK@users.noreply.github.com>
2020-10-17 12:31:46 -0500

    Update HealthLife.sol

--- Contracts/HealthLife.sol
+++ Contracts/HealthLife.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract GustavoCoin is ERC20Mintable {
+contract HealthLifeCoin is ERC20Mintable {
     string public name = "HEALTH LIFE COIN";
     string public symbol = "HLC";
     uint8 public decimals = 18;
