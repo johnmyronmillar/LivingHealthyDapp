8a7338969a72f9a4ef39a98a81df2e0977c9f096
Richard Henderson <64328714+HendersonRichardK@users.noreply.github.com>
2020-10-19 19:47:47 -0500

    Update HealthMarket.sol

--- Contracts/HealthMarket.sol
+++ Contracts/HealthMarket.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.6.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import './HealthAuction.sol';
+import './LivingHealthyAuction.sol';
 
-contract HealthMarket is ERC721, Ownable {
-    constructor() ERC721("HealthMarket", "MARS") public {}
+contract LivingHealthyMarket is ERC721, Ownable {
+    constructor() ERC721("LivingHealthyMarket", "MARS") public {}
 
     address payable foundationAddress = msg.sender;
 
-    mapping(uint => HealthAuction) public auctions;
+    mapping(uint => LivingHealthyAuction) public auctions;
 
     function registerLand(string memory tokenURI) public payable onlyOwner {
         uint _id = totalSupply();
@@ -19,41 +19,41 @@ contract HealthMarket is ERC721, Ownable {
     }
 
     function createAuction(uint tokenId) public onlyOwner {
-        auctions[tokenId] = new HealthAuction(foundationAddress);
+        auctions[tokenId] = new LivingHealthyAuction(foundationAddress);
     }
 
     function endAuction(uint tokenId) public onlyOwner {
         require(_exists(tokenId), "Land not registered!");
-        HealthAuction auction = getAuction(tokenId);
+        LivingHealthyAuction auction = getAuction(tokenId);
         auction.auctionEnd();
         safeTransferFrom(owner(), auction.highestBetter(), tokenId);
     }
 
-    function getAuction(uint tokenId) public view returns(HealthAuction auction) {
+    function getAuction(uint tokenId) public view returns(LivingHealthyAuction auction) {
         return auctions[tokenId];
     }
 
     function auctionEnded(uint tokenId) public view returns(bool) {
         require(_exists(tokenId), "Land not registered!");
-        HealthAuction auction = getAuction(tokenId);
+        LivingHealthyAuction auction = getAuction(tokenId);
         return auction.ended();
     }
 
     function highestBet(uint tokenId) public view returns(uint) {
         require(_exists(tokenId), "Land not registered!");
-        HealthAuction auction = getAuction(tokenId);
+        LivingHealthyAuction auction = getAuction(tokenId);
         return auction.highestBet();
     }
 
     function pendingReturn(uint tokenId, address sender) public view returns(uint) {
         require(_exists(tokenId), "Land not registered!");
-        HealthAuction auction = getAuction(tokenId);
+        LivingHealthyAuction auction = getAuction(tokenId);
         return auction.pendingReturn(sender);
     }
 
     function Bet(uint tokenId) public payable {
         require(_exists(tokenId), "Land not registered!");
-        HealthAuction auction = getAuction(tokenId);
+        LivingHealthyAuction auction = getAuction(tokenId);
         auction.Bet.value(msg.value)(msg.sender);
     }
 
