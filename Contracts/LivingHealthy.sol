9b4c125537077a8d2f71466ece9d520749019063
Richard Henderson <64328714+HendersonRichardK@users.noreply.github.com>
2020-10-19 19:48:50 -0500

    Update and rename HealthLifeCoin.sol to LivingHealthy.sol

--- Contracts/LivingHealthy.sol
+++ Contracts/LivingHealthy.sol
@@ -4,7 +4,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract HealthLifeCoin is ERC20, ERC20Detailed, ERC20Mintable {
+contract LivingHealthyCoin is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
         string memory name,
         string memory symbol,
